717d6d9e3e7aa631fe785e3c87e44f79209bd063
codingkjy <codingkjy28@gmail.com>
2022-11-13 23:06:06 +0900

    dd

---
+++ kakao-login-server/src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Post, Body, Response, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ){}
+    @Post('/login')
+    async login(@Body() body: any, @Response() res): Promise<any> {
+        try {
+        const { code, domain } = body;
+        if (!code || !domain) {
+            throw new BadRequestException('카카오 정보가 없습니다.');
+        }
+        const kakao = await this.authService.kakaoLogin({ code, domain });
+
+        console.log(`kakaoUserInfo : ${JSON.stringify(kakao)}`);
+        if (!kakao.id) {
+            throw new BadRequestException('카카오 정보가 없습니다.');
+        }
+
+        res.send({
+            user: kakao,
+            message: 'success',
+        });
+        } catch (e) {
+            console.log(e);
+            throw new UnauthorizedException();
+        }
+    }
+}
---
+++ kakao-login-server/src/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import axios from 'axios';
+import * as qs from 'qs';
+
+@Injectable()
+export class AuthService {
+    async kakaoLogin(options: { code: string; domain: string }): Promise<any> {
+        const { code, domain } = options;
+        const kakaoKey = 'ebf97db0c2564aa4f0575fd49c0f2e24';
+        const kakaoTokenUrl = 'https://kauth.kakao.com/oauth/token';
+        const kakaoUserInfoUrl = 'https://kapi.kakao.com/v2/user/me';
+        const body = {
+          grant_type: 'authorization_code',
+          client_id: kakaoKey,
+          redirect_uri: `${domain}/kakao-callback`,
+          code,
+        };
+        console.log(body);
+        const headers = {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        };
+        try {
+          const response = await axios({
+            method: 'POST',
+            url: kakaoTokenUrl,
+            timeout: 30000,
+            headers,
+            data: qs.stringify(body),
+          });
+          console.log(response.data)
+          const headerUserInfo = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+             Authorization: 'Bearer ' + response.data.access_token,
+          };
+          const responseUserInfo = await axios({
+            method: 'GET',
+            url: kakaoUserInfoUrl,
+            timeout: 30000,
+            headers: headerUserInfo,
+          });
+          return responseUserInfo.data;
+        } catch (error) {
+          console.log(error);
+          throw new UnauthorizedException();
+        }
+      }
+}
